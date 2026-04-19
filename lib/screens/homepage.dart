2dc0eb96ea96a0855b353ed86c5e43e74369741e
wtvrr <nisa_cosk@icloud.com>
2026-04-19 19:44:42 +0200

    - added homescreen as well as the menu screen - added logo and button for the next slide

---
+++ lib/screens/homepage.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatelessWidget {
+  const Homepage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 226, 97, 50),
+
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          //OBEN (Logo mittig)
+          Padding(
+            padding: const EdgeInsets.only(top: 80),
+            child: Center(
+              child: Image.asset(
+                'assets/ohnekaffee.png',
+                width: 230,
+              ),
+            ),
+          ),
+
+          // UNTEN (Button mittig)
+          Padding(
+            padding: const EdgeInsets.only(bottom: 40),
+            child: Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/menupage');
+                },
+                child: const Text("Zum Menü"),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
